cf28da01b8e87818e9532a6e863b300079a30400
Marco Cajeao <mcajeao@westnet.com.ar>
2022-10-18 23:10:32 -0300

    Initial version of the subjects module

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('settings.index') }}">{{ __('Settings') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('subjects.index') }}">{{ __('Subjects') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('settings', App\Http\Controllers\SettingController::class);
+Route::resource('subjects', App\Http\Controllers\SubjectController::class);

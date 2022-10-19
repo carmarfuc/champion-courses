296ec5f9a64df165d0ab2627f80097a683acdb22
Marco Cajeao <mcajeao@westnet.com.ar>
2022-10-18 23:04:24 -0300

    Initial version of the settings module

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('settings.index') }}">{{ __('Settings') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
